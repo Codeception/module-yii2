a2daff5b6563ad0ca82d80b05ecc8a3462182533
DBX12 <dbx12@dbx12.de>
2021-10-26 08:20:31 +0200

    Correct paths used in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ defined('YII_DEBUG') || define('YII_DEBUG', true);
 defined('YII_ENV') || define('YII_ENV', 'test');
 
 call_user_func(function() {
-    $loader = require __DIR__ . '/vendor/autoload.php';
+    $loader = require __DIR__ . '/../vendor/autoload.php';
 
     $container = new \yii\di\Container();
 //    call_user_func(function() use ($container) {
@@ -14,7 +14,7 @@ call_user_func(function() {
     Yii::$container = $container;
 });
 
-$link = __DIR__ . '/vendor/yiisoft/yii2-app-advanced/vendor';
-if (!file_exists($link) && !symlink(__DIR__ . '/vendor', $link)) {
+$link = __DIR__ . '/../vendor/yiisoft/yii2-app-advanced/vendor';
+if (!file_exists($link) && !symlink(__DIR__ . '/../vendor', $link)) {
     die('failed to create symlink');
 }
--- tests/cases/yii2-app-advanced/config.php
+++ tests/cases/yii2-app-advanced/config.php
@@ -1,12 +1,12 @@
 <?php
 
-file_put_contents(__DIR__ . '/../../vendor/yiisoft/yii2-app-advanced/common/config/params-local.php', '<?php return [];');
-file_put_contents(__DIR__ . '/../../vendor/yiisoft/yii2-app-advanced/frontend/config/params-local.php', '<?php return [];');
+file_put_contents(__DIR__ . '/../../../vendor/yiisoft/yii2-app-advanced/common/config/params-local.php', '<?php return [];');
+file_put_contents(__DIR__ . '/../../../vendor/yiisoft/yii2-app-advanced/frontend/config/params-local.php', '<?php return [];');
 
 // Copy the database to the output dir so we don't get changes in version control.
 copy(__DIR__ . '/_data/db.sqlite', codecept_output_dir('/db.sqlite'));
 
-$config = \yii\helpers\ArrayHelper::merge(require __DIR__ . '/../../vendor/yiisoft/yii2-app-advanced/frontend/config/main.php', [
+$config = \yii\helpers\ArrayHelper::merge(require __DIR__ . '/../../../vendor/yiisoft/yii2-app-advanced/frontend/config/main.php', [
     'components' => [
         'db' => [
             'class' => yii\db\Connection::class,
@@ -26,7 +26,7 @@ $config = \yii\helpers\ArrayHelper::merge(require __DIR__ . '/../../vendor/yiiso
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset'
     ],
-    'vendorPath' => __DIR__ . '/../../vendor'
+    'vendorPath' => __DIR__ . '/../../../vendor'
 ]);
 
 return $config;
