024339c4fb216d0a70e82926f125920c93134e7a
DBX12 <dbx12@dbx12.de>
2021-10-27 11:10:15 +0200

    Remove commented code and unused variable $loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,17 +2,7 @@
 defined('YII_DEBUG') || define('YII_DEBUG', true);
 defined('YII_ENV') || define('YII_ENV', 'test');
 
-call_user_func(function() {
-    $loader = require __DIR__ . '/../vendor/autoload.php';
-
-    $container = new \yii\di\Container();
-//    call_user_func(function() use ($container) {
-//        require __DIR__ .'/../src/config/di.php';
-//    });
-
-//    Yii::$loader = $loader;
-    Yii::$container = $container;
-});
+Yii::$container = new \yii\di\Container();
 
 $link = __DIR__ . '/../vendor/yiisoft/yii2-app-advanced/vendor';
 if (!file_exists($link) && !symlink(__DIR__ . '/../vendor', $link)) {
