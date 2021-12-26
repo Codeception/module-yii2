c8b98abd58316bdb9483ba564b38e47b071d0735
TavoNiievez <ganieves@outlook.com>
2021-12-26 16:43:34 -0500

    Fix amOnRoute vs amOnPage

--- tests/cases/mock-mailer/functional/MockMailerCest.php
+++ tests/cases/mock-mailer/functional/MockMailerCest.php
@@ -12,14 +12,14 @@ final class MockMailerCest
 
     public function testCountMailSentWithoutRedirect(FunctionalTester $I)
     {
-        $I->amOnPage(['site/send-mail-without-redirect']);
+        $I->amOnPage('site/send-mail-without-redirect');
 
         $I->seeEmailIsSent(1);
     }
 
     public function testCountMailSentWithRedirect(FunctionalTester $I)
     {
-        $I->amOnPage(['site/send-mail-with-redirect']);
+        $I->amOnPage('site/send-mail-with-redirect');
 
         $I->seeEmailIsSent(1);
     }
