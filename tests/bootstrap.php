df4e36bd3c8201d5a37c091eb20ae72aed20efee
Luke English <luke.english@ec2i.biz>
2022-02-23 13:49:15 +0000

    bootstrap updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,13 @@
 defined('YII_DEBUG') || define('YII_DEBUG', true);
 defined('YII_ENV') || define('YII_ENV', 'test');
 
-Yii::$container = new \yii\di\Container();
+call_user_func(function() {
+    require dirname(__DIR__) . '/vendor/autoload.php';
+    $container = new \yii\di\Container();
+    Yii::$container = $container;
+});
 
-$link = __DIR__ . '/../vendor/yiisoft/yii2-app-advanced/vendor';
-if (!file_exists($link) && !symlink(__DIR__ . '/../vendor', $link)) {
+$link = dirname(__DIR__) . '/vendor/yiisoft/yii2-app-advanced/vendor';
+if (!file_exists($link) && !symlink(dirname(__DIR__) . '/vendor', $link)) {
     die('failed to create symlink');
 }
