bfd2c8a3df22bbecd1b998cb9ecddbd27ed23a21
Luke English <luke.english@ec2i.biz>
2022-02-23 22:00:44 +0000

    removing closure from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,9 @@
 defined('YII_DEBUG') || define('YII_DEBUG', true);
 defined('YII_ENV') || define('YII_ENV', 'test');
 
-call_user_func(function() {
-    require dirname(__DIR__) . '/vendor/autoload.php';
-    $container = new \yii\di\Container();
-    Yii::$container = $container;
-});
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+Yii::$container = new \yii\di\Container();
 
 $link = dirname(__DIR__) . '/vendor/yiisoft/yii2-app-advanced/vendor';
 if (!file_exists($link) && !symlink(dirname(__DIR__) . '/vendor', $link)) {
